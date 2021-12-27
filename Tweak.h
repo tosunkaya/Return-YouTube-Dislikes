ae9d1fb5ac5b8ef93a5935af36dd0e74f97e8fd7
PoomSmart <thatchapon.unp@outlook.com>
2021-12-27 11:25:14 +0700

    Transitioning to native logic and supporting toggling dislike button

--- Tweak.h
+++ Tweak.h
@@ -1,27 +1,48 @@
+#import <Foundation/Foundation.h>
+
 @interface YTAppDelegate : UIResponder <UIApplicationDelegate>
 @end
 
 @interface YTSlimVideoScrollableDetailsActionsView : UIView
 @end
 
-@interface YTFormattedStringLabel : UILabel
+@interface YTIFormattedString : NSObject
++ (instancetype)formattedStringWithString:(NSString *)string;
+@end
+
+@interface YTIFormattedStringLabel : UILabel
+- (void)setFormattedString:(YTIFormattedString *)string;
 @end
 
 @interface YTQTMButton : UIButton
 @end
 
+@interface YTIToggleButtonRenderer : NSObject
+@property (nonatomic, strong, readwrite) YTIFormattedString *defaultText;
+@property (nonatomic, strong, readwrite) YTIFormattedString *toggledText;
+@end
+
+@interface YTIButtonSupportedRenderers : NSObject
+@property (nonatomic, strong, readwrite) YTIToggleButtonRenderer *toggleButtonRenderer;
+@end
+
+
 @interface YTSlimVideoDetailsActionView : UIView
+@property (nonatomic, strong, readwrite) YTIFormattedStringLabel *label;
 - (instancetype)initWithSlimMetadataButtonSupportedRenderer:(id)renderer;
 @end
 
+@interface YTILikeTarget : NSObject
+@property (nonatomic, copy, readwrite) NSString *videoId;
+@end
+
+@interface YTISlimMetadataToggleButtonRenderer : NSObject
+@property (nonatomic, strong, readwrite) YTILikeTarget *target;
+@property (nonatomic, strong, readwrite) YTIButtonSupportedRenderers *button;
+@end
+
 @interface YTISlimMetadataButtonSupportedRenderers : NSObject
+@property (nonatomic, strong, readwrite) YTISlimMetadataToggleButtonRenderer *slimMetadataToggleButtonRenderer;
+- (BOOL)slimButton_isDislikeButton;
 - (int)rendererOneOfCase;
-@end
-
-@interface YTLocalPlaybackController : NSObject
-- (NSString *)currentVideoID;
-@end
-
-@interface YTUserDefaults : NSObject
-- (long long)appThemeSetting;
 @end
